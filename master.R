aee99612316c9ab0fdf151e5a104f186f30148db
Harris <c.a.harris@uu.nl>
2018-06-28 14:27:59 +0200

    Add constants, examples, strategy

--- master.R
+++ master.R
@@ -2,16 +2,43 @@
 # later this will be the controller for all simulations
 # Differentiate somewhere between 
 
+### Setup----
+
 # Load dependencies
 if (!require(needs)) {install.packages("needs"); library(needs)}
 needs(dplyr, magrittr)
 
+### Functions----
+source("Strategies/guessing.R")
+source("Strategies/weighting.R")
+source("Strategies/window.R")
+
+### Constants----
+
+# Set up sine curves
+x <- seq(0,pi*2*5,by = .1) # Trials
+var <- 2 # Amplitude of sine curve
+range <- max(max(abs(.2*x+var*sin(x))),max(abs(-.2*x+var*sin(x))))
+
+# Positive
+CURVE_A_pos <- (0.2*x+var*sin(x))/(2*range)+.5 # Sine curve for option A
+CURVE_B_pos <- (0.2*x+var*sin(x+pi))/(2*range)+.5 # Sine curve for option B
+# Neutral
+CURVE_A_neu <- (var*sin(x))/(2*range)+.5 # Sine curve for option A
+CURVE_B_neu <- (var*sin(x+pi))/(2*range)+.5 # Sine curve for option B
+# Negative
+CURVE_A_neg <- (-0.2*x+var*sin(x))/(2*range)+.5 # Sine curve for option A
+CURVE_B_neg <- (-0.2*x+var*sin(x+pi))/(2*range)+.5 # Sine curve for option B
+
 
 # Create practice dataset
-df <- rbind(rep("wA", 6),
-            rep("lA", 6),
-            rep(c("lA", "lB"), 3),
-            rep(c("wA", "wB"), 3)) %>%
+df <- rbind(rep("wA", 12),
+            rep("lA", 12),
+            rep(c("lA", "lB"), 6),
+            rep(c("wA", "wB"), 6),
+            rep(c("wA", "lA", "wB", "lB"), 3),
+            rep(c("wA", "wA", "lB"), 4),
+            rep(c("lB", "wB", "lB", "wA"), 3)) %>%
   as_tibble
 
 
@@ -22,18 +49,23 @@ strat_window(df, 1)
 strat_window(df, 2)
 
 # Weighting
-strat_weight(df, .1) # These two return equal results
-strat_weight(df, .9) # Check function
+strat_weight(df, 0)
+strat_weight(df, .1)
+strat_weight(df, .9)
+strat_weight(df, 1)
 
 # Guessing
 strat_guessing(df)
 
+# Omniscient
+strat_omni(df, CURVE_A_neu, CURVE_B_neu)
+
 ### List of strategies
 # Window :)
-# Recency weighting
+# Recency weighting :)
 # Constructivist
 # Guess :)
-# Omniscient
+# Omniscient :)
 
 ### Code snippet for resolving guesses
 
