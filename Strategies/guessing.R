4ebf88e9ab285be51cf6ce17d1635e882ff38c66
Harris <c.a.harris@uu.nl>
2018-06-28 14:27:34 +0200

    Simplified guessing strategy

--- Strategies/guessing.R
+++ Strategies/guessing.R
@@ -1,9 +1,8 @@
 ### Guessing
 strat_guessing <- function(dfguess) {
-  # Tibble to store expected choices
-  dfexpectations <- matrix(nrow = nrow(dfguess), ncol = ncol(dfguess)) %>%
-    as_tibble()
-  # Fill with guesses
-  apply(dfexpectations, 2, function(x) {rep("Guess", nrow(dfexpectations))}) %>%
+  # Fill with guesses and return
+  rep("Guess", nrow(dfguess)) %>%
+    matrix(nrow = nrow(dfguess), byrow = TRUE) %>%
+    as_tibble %>%              
     return
-}
+}    
