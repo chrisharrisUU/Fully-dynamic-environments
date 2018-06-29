74af61604afb5234b28f684799ce3b18b93660e2
Harris <c.a.harris@uu.nl>
2018-06-29 16:55:02 +0200

    Rewrote to run single trials
    This allows the functions to be used more easily both for matching and for simulating

--- Strategies/guessing.R
+++ Strategies/guessing.R
@@ -1,8 +1,6 @@
 ### Guessing
 strat_guessing <- function(dfguess) {
   # Fill with guesses and return
-  rep("Guess", nrow(dfguess)) %>%
-    matrix(nrow = nrow(dfguess), byrow = TRUE) %>%
-    as_tibble %>%              
+  rep("Guess", dfguess) %>%
     return
 }    
--- Strategies/omniscient.R
+++ Strategies/omniscient.R
@@ -1,11 +1,3 @@
-strat_omni <- function(dfomni, acurve, bcurve) {
- # Determine best choice
-  best_choice <- tibble(acurve, bcurve) %>%
-    transmute(best = ifelse(acurve > bcurve, "A", ifelse(acurve == bcurve, "Guess", "B"))) %>% unlist
-  
-  # Fill with guesses and return
-  rep(best_choice, nrow(dfomni)) %>%
-    matrix(nrow = nrow(dfomni), byrow = TRUE) %>%
-    as_tibble %>%
-    return
+strat_omni <- function(dfomni, best) {
+ rep(best[dfomni[2]], dfomni[1])
 }
