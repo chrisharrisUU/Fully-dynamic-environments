fe7f0b90b0634e3ab0e781196f166138e0de1f1f
Chris Harris <c.a.harris@uu.nl>
2019-01-11 17:08:15 +0100

    Include simulation

--- master.R
+++ master.R
@@ -144,3 +144,77 @@ tibble(x = 1:14,
        title = "Mean number of guesses for positivist strategy") +
   scale_color_brewer(palette = "Dark2") +
   theme_minimal()
+
+
+
+### Simulation----
+# Most successful strategy
+
+n <- 100 # number of "participants"
+c <- 1 # counter
+strats <- c("positivist", "constructivist", "guessing", "omniscient") # strategies
+envs <- c("positive", "neutral", "negative") # environments
+winds <- c(1:14, 20, 40, 60, 80, 100) # Window sizes
+success_results <- tibble(strategy = "test",
+                          environment = "neutral",
+                          window = 0,
+                          guesses = 0,
+                          success = 0) # Tibble for outcomes
+# Set up progress bar
+progress <- txtProgressBar(1, length(strats) * length(envs) * length(winds), style = 3)
+
+for (s in strats) {
+  for (env in envs) {
+    for (w in winds) {
+      # Run simulation
+      result <- sim_handler_guesses(strategy = s,
+                                    environment = env,
+                                    strat_var = w,
+                                    size = c(n, length(x)))
+      # Average number of guesses
+      nr_guesses <- result[[3]] %>%
+        rowSums(na.rm = TRUE) %>%
+        mean
+      # Average number of successful outcomes
+      nr_success <- result[[1]] %>%
+        transmute_all(.funs = funs(grepl("w", .))) %>%
+        transmute_all(.funs = funs(ifelse(., 1, 0))) %>%
+        rowSums() %>%
+        mean
+      # Save in tibble
+      success_results[c,] <- c(s, env, w, nr_guesses, nr_success)
+      
+      # Show progress bar
+      setTxtProgressBar(progress, c)
+      # Update counter
+      c <- c + 1
+    }
+  }
+}
+
+# Prepare results
+success_results %<>%
+  mutate(strategy = factor(strategy, levels = strats),
+         environment = factor(environment, levels = envs),
+         window = factor(window, levels = winds),
+         guesses = as.numeric(guesses),
+         success = as.numeric(success))
+
+save(success_results, file = "Output/sim_results.RData")
+
+# Descriptive
+success_results %>%
+  group_by(strategy, environment) %>%
+  summarize(max = max(success),
+            min = min(success),
+            avg = mean(success))
+
+# Graph
+success_results %>%
+  ggplot(aes(x = window,
+             y = success,
+             shape = environment,
+             color = strategy)) +
+  geom_point() +
+  scale_color_brewer(palette = "Dark2") +
+  theme_minimal()
